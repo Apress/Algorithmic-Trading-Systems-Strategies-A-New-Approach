6160d76056fa85aeb8fad2745d2378f424d23f7d
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2024-01-30 13:17:20 -0500

    Add files via upload

---
+++ Chapter7/OptimizationAlgorithms/OptimizationAlgorithmsCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OptimizationAlgorithms.Algorithms.BruteForce;
+using OptimizationAlgorithms.Algorithms.Genetic;
+using OptimizationAlgorithms.Common;
+
+namespace OptimizationAlgorithms;
+
+public static class OptimizationAlgorithmsCollectionExtensions
+{
+public static IServiceCollection AddOptimizationAlgorithms(this IServiceCollection services)
+{
+    services.AddSingleton<AlgorithmFactory>();
+
+    services.AddBruteForceAlgorithm();
+    services.AddGeneticAlgorithm();
+
+    return services;
+}
+}
---
+++ Chapter7/OptimizationAlgorithms/TestFunctions/RastriginFunction.cs
@@ -0,0 +1,18 @@
+﻿using OptimizationAlgorithms.Common;
+
+namespace OptimizationAlgorithms.TestFunctions;
+
+public static class RastriginFunction
+{
+    public static decimal Calculate(List<FunctionVariable> functionVariables, AlgorithmPoint point)
+    {
+        double result = 0;
+        foreach (FunctionVariable variable in functionVariables)
+        {
+            double x = (double)point.Values.First(v => v.Id == variable.Id).Value;
+            result += x * x - 10 * Math.Cos(2 * Math.PI * x) + 10;
+        }
+
+        return (decimal)result;
+    }
+}
